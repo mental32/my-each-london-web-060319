39a22e1ce36c8dc7bdecb1f7b081553af711b6b6
beatific-revision-5216 <ivanov_chris@yahoo.com>
2019-05-29 04:47:00 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -1,6 +1,13 @@
 def my_each # put argument(s) here
   if has_block?
-    yield(part)
+
+    step = 0
+    limit = s.size
+
+    while step != limit do
+      yield(part)
+      step += 1
+    end
   end
 
   s
